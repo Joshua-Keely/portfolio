78c54fbcbb2754a8f9809fa8c85228a575153c31
keelyj <joshua.keely@etu.univ-grenoble-alpes.fr>
2022-01-21 15:26:26 +0100

    Update script

--- alwaysdata.sh
+++ alwaysdata.sh
@@ -7,20 +7,6 @@ rm -rf keelyj.zip
 zip -r keelyj .
 # 3. Transfer .zip to Always Data WebDAV
 curl -T keelyj.zip -u keelyj webdav-keelyj.alwaysdata.net/
-# 4. Connect to web server with SSH
-ssh keelyj@ssh-keelyj.alwaysdata.net
-# 5.  Move .zip to www
-mv keelyj.zip www
-cd www
-# 6. Unzip Directory in www
-unzip -r keelyj.zip 
-# 7. Remove old files from www
-rm -rf css cv img index.html projets saes scripts
-# 8. Move files from master Directory to www
-cd keelyj
-mv -t ../ css cv img index.html saes scripts projets 
-# 9. Delete empty directory and .zip 
-rm -rf keelyj keelyj.zip
 #END OF SCRIPT !
 
 
